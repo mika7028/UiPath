f57b64fbb432346c974466e5085e1f9990271670
三ヶ島 雅彦 <mikashima.m@value.b-style.net>
2019-06-17 13:27:04 +0900

    変更したよ

---
+++ pushまで.bat
@@ -0,0 +1,10 @@
+cd C:\github\UiPath
+git add -A
+git commit -m "•ÏX‚µ‚½‚æ"
+rem Å‰‚Ì‚İ•K—v
+rem git remote add origin https://github.com/mika7028/UiPath
+git push origin master
+mika7028@gmail.com
+fgfhsGGsfjfj&6hfhjsh
+pause
+
